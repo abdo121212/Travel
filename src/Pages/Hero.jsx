951f69a0a90a017ae8e76e6bb5615834e6cf7e7c
Abdallah Atef <atfbdallh147@gmail.com>
2025-04-30 04:02:48 +0300

    add simple style

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
                 </div>
               </div>
             </div>
-            <button className="bg-gradient-to-r from-primary to-secondary text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -bottom-5 left-1/2 -translate-x-1/2">
+            <button className="bg-gradient-to-r  to-[#0287a8] from-[#00c3c7] text-white hover:scale-105 px-4 py-2 rounded-full duration-200 absolute -bottom-5 left-1/2 -translate-x-1/2">
               Search Now
             </button>
           </div>
